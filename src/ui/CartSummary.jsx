a9e3f6d085cee0e8a31eaba8c478edad055ea250
Leonardo Cunha <leo.francasp97@gmail.com>
2024-08-23 10:49:33 -0300

    Centralizando verticalmente "Total Order" no modal

--- src/ui/CartSummary.jsx
+++ src/ui/CartSummary.jsx
@@ -1,6 +1,6 @@
 function CartSummary({ total }) {
   return (
-    <div className="flex justify-between">
+    <div className="flex items-center justify-between">
       <p className="text-sm">Order Total</p>
       <p className="text-xl font-bold text-primary-950">{total}</p>
     </div>
